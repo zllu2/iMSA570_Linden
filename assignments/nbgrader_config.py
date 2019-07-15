02be95de9d34b6c5e9bd7a3dce043997f336cf15
Linden Lu <lindenlu@MacBook-Pro-8.attlocal.net>
2019-07-15 02:34:21 -0500

    fix assignment problems

--- assignments/nbgrader_config.py
+++ assignments/nbgrader_config.py
@@ -405,7 +405,7 @@
 #c.ClearSolutions.begin_solution_delimeter = 'BEGIN SOLUTION'
 
 ## The code snippet that will replace code solutions
-#c.ClearSolutions.code_stub = {'python': '# YOUR CODE HERE\nraise NotImplementedError()'}
+c.ClearSolutions.code_stub = {'python': '# YOUR CODE HERE\n'}
 
 ## The delimiter marking the end of a solution
 #c.ClearSolutions.end_solution_delimeter = 'END SOLUTION'
@@ -552,7 +552,7 @@
 ## Removes the output from all code cells in a notebook.
 
 ## 
-#c.ClearOutputPreprocessor.remove_metadata_fields = {'collapsed', 'scrolled'}
+#c.ClearOutputPreprocessor.remove_metadata_fields = {'scrolled', 'collapsed'}
 
 #------------------------------------------------------------------------------
 # ClearOutput(NbGraderPreprocessor,ClearOutputPreprocessor) configuration
